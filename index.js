b8885a237f38564b7edbab0de8226a066f7f727d
mac <mac@Kalids-MacBook-Pro.local>
2023-10-27 15:34:09 +0300

    more edits

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000
 
 const baseURL = 'https://api.edamam.com'
-const API_ID = '85a188c7'
+const APP_ID = '85a188c7'
 const APP_KEY = 'a8625290af811d0f10308ebce327e566'
 
 app.use(express.static("public"));
@@ -20,14 +20,23 @@ app.get("/", (req, res) => {
 app.post("/food-detail", async (req, res) => {
     
     try {
-        const response = await axios.get(baseURL + )
+        const response = await axios.get(baseURL + `/api/food-database/v2/parser?app_id=${APP_ID}&app_key=${APP_KEY}&ingr=${req.body.query}`)
+        const result = response.data
 
+        console.log(result)
+
+        res.render("food-detail.ejs", {
+            text: result.text,
+            details: result.hints
+        })
+        
     } catch (error) {
         console.log(error.response.data)
-        res.status(404)
+        res.render("index.ejs", {
+            errors: error.response.data
+        })
     }
 })
-
 
 app.listen(port, (req, res) => {
     console.log("Server running on port: " + port)
